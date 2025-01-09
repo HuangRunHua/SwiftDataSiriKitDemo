d891b3f044db0601b9efacb2bc0207a8980939b5
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2025-01-09 17:30:31 +0800

    Initial Commit

---
+++ SwiftDataSiriKitDemo/SwiftDataSiriKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataSiriKitDemoApp.swift
+//  SwiftDataSiriKitDemo
+//
+//  Created by Runhua Huang on 2025/1/9.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataSiriKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
